655e5e53cbf678110cb28091b8549dc86b8432b0
Thomas Felix <thomasfelixbrilliant@gmail.com>
2022-04-07 03:58:33 +0700

    fix: company page view

--- resources/views/pages/user/company.blade.php
+++ resources/views/pages/user/company.blade.php
@@ -9,17 +9,17 @@
     <div class="text-center">
         <h1 class="display-3 fw-bold">About Us</h1>
         <hr class="bg-white mb-3 mt-0 d-inline-block mx-auto" style="width: 100px; height:3px;">
-        <p class="p-text text-white mb-5">Profile of BlockDev</p>
+        <h4 class="p-text text-white mb-5">Profile of BlockDev</h4>
     </div>
     <div class="row">
-        <h6 class="text-center mt-4 text-light h6">BlockDev is a startup that specializes in creating WordPress plugins and themes. Hundreds of well-known clients such as SemenGresik, Desa Oro-Oro Ombo and many others choose us as partners to improve their websites to achieve the best results. BlockDev is able to meet your expectations in perfecting the features of your start-up web development including planning, system design, content, prototypes, UI/UX, neat code implementation, infrastructure and testing. Everything will work according to your expectations to provide features in your startup website.</h5>
+        <div class="text-center mt-4 text-light" style="font-size: 20px">BlockDev is a startup that specializes in creating WordPress plugins and themes. Hundreds of well-known clients such as SemenGresik, Desa Oro-Oro Ombo and many others choose us as partners to improve their websites to achieve the best results. BlockDev is able to meet your expectations in perfecting the features of your start-up web development including planning, system design, content, prototypes, UI/UX, neat code implementation, infrastructure and testing. Everything will work according to your expectations to provide features in your startup website.</div>
     </div>
 </section>
 <section id="our-partners">
     <div class="text-center">
         <h1 class="display-3 fw-bold">Our Partners</h1>
         <hr class="bg-white mb-3 mt-0 d-inline-block mx-auto" style="width: 100px; height:3px;">
-        <p class="p-text text-white mb-5">List of companies collaborating with BlockDev</p>
+        <h4 class="p-text text-white mb-5">Pleasure to work with them</h4>
     </div>
     <div class="container h-100">
         <div class="row align-items-center h-100">
@@ -48,7 +48,7 @@
     <div class="text-center">
         <h1 class="display-3 fw-bold">Meet Our Team</h1>
         <hr class="bg-white mb-3 mt-0 d-inline-block mx-auto" style="width: 100px; height:3px;">
-        @* <p class="p-text text-white mb-5">Profile of BlockDev</p> *@
+        <h4 class="p-text text-white mb-5">These are the people that make the magic happen</h4>
     </div>
     <div class="container mt-4">
         <div class="row">
@@ -96,8 +96,8 @@
     <div class="row text-center mt-5">
         <h1>Interested working on BlockDev?</h1>
         <div class="d-flex justify-content-center">
-            <div class="col-md-4 mt-5">
-                <button type="button" class="btn btn-lg btn-outline-light btn-rounded">Check out our available positions</button>
+            <div class="col-md-4 mt-4">
+                <a href="#" class="btn btn-lg btn-outline-light btn-rounded">Check it out!</a>
             </div>
         </div>
     </div>
